56273ce2ae8484d29fa9fbbb1713114106109e9a
duranto2009 <12559078+duranto2009@users.noreply.github.com>
2023-11-04 01:42:49 +0600

    First release

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts', [ 'posts' => Post::latest()->get() ]);
+});
+
+Route::get('posts/{post:slug}', function(Post $post){
+    return view('post', [ 'post' => $post]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category){
+    return view('posts', [ 'posts' => $category->posts ]);
+});
+Route::get('authors/{author:username}', function (User $author){
+    return view('posts', [ 'posts' => $author->posts ]);
+});
